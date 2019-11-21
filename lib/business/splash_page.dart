51e4a49c4c068f837a2f03476ea3353ee8bf95c1
PollexLee <l6376751@gmail.com>
2019-11-21 09:35:41 +0800

    * 添加splash页面

---
+++ lib/business/splash_page.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return null;
+  }
+}
+
+class SplashState extends State<SplashPage>{
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+
+}
